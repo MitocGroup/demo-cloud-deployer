9217527dd505ae3be01bd011102f0e4a15aec72b
Eugene I <eugene@mitocgroup.com>
2019-11-11 14:48:30 -0500

    minor improvement

--- bin/create-ssm.sh
+++ bin/create-ssm.sh
@@ -1,8 +1,7 @@
-for ((i = 51; i <= 100; i++)); do
+for ((i = 1; i <= 100; i++)); do
+   echo "${i}"
 #   terrahub component --name "component_${i}" --template aws_ssm_parameter
 #   terrahub configure --include "component_${i}" -c component.template.tfvars.component_${i}_name="component_${i}"
 #   terrahub configure --include "component_${i}" -c component.template.tfvars.component_${i}_type="String"
 #   terrahub configure --include "component_${i}" -c component.template.tfvars.component_${i}_value="component_${i}"
-
-   echo "${i}"
 done
